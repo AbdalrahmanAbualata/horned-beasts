e666f8c3dc0c0bb42d483a282d7459d9ec975275
AbdalrahmanAbualata <abuataabooood@yahoo.com>
2021-08-08 19:12:15 +0300

    class01

---
+++ src/components/HornedBeasts.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+
+  render() {
+
+    return (
+
+      <div className='apples'>
+        <h2> {this.props.title} </h2>
+        <h3> {this.props.keyword}</h3>
+        <h3> {this.props.horns} </h3>
+        <img src={this.props.imageUrl} alt={this.props.imageAlt} title={this.props.imageTitle}></img>
+        <p> {this.props.description} 
+        </p>
+
+      </div>
+
+    );
+  }
+}
+
+export default HornedBeast;
